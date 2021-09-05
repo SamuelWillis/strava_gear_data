60e9923053fc89a6b253e75cf1276cf61ab3cfd3
SamuelWillis <samuel.w.h.willis@gmail.com>
2021-09-05 15:17:01 -0600

    Display Gear on GearLive
    Display the authorized athlete's gear on the `GearLive` page once it is
    persisted to the DB.

--- lib/strava_gear_data_web/live/gear_live.ex
+++ lib/strava_gear_data_web/live/gear_live.ex
@@ -2,10 +2,23 @@ defmodule StravaGearDataWeb.GearLive do
   @moduledoc false
   use StravaGearDataWeb, :live_view
 
-  @impl true
-  def render(assigns) do
-    ~L"""
-    <h1>Hello</h1>
-    """
+  alias StravaGearData.Athletes
+  alias StravaGearData.Gear
+
+  @impl Phoenix.LiveView
+  def mount(_params, %{"token" => token}, socket) do
+    {:ok, athlete_id} =
+      Phoenix.Token.verify(StravaGearDataWeb.Endpoint, "athlete auth", token, max_age: :infinity)
+
+    athlete = Athletes.get_athlete!(athlete_id)
+
+    gear = Gear.get_for!(athlete)
+
+    socket =
+      socket
+      |> assign(:athlete, athlete)
+      |> assign(:gear, gear)
+
+    {:ok, socket}
   end
 end
--- test/strava_gear_data_web/live/gear_live_test.exs
+++ test/strava_gear_data_web/live/gear_live_test.exs
@@ -1,14 +1,38 @@
 defmodule StravaGearDataWeb.GearLiveTest do
-  use StravaGearDataWeb.ConnCase
+  use StravaGearDataWeb.ConnCase, async: true
 
   import Phoenix.LiveViewTest
 
-  test "disconnected and connected render", %{conn: conn} do
-    conn = Phoenix.ConnTest.init_test_session(conn, %{token: "my fake token"})
+  setup :authorize_athlete
 
-    {:ok, gear_live, disconnected_html} = live(conn, "/")
+  describe "disconnected and connected mount" do
+    test "renders message when no athlete gear", %{conn: conn, athlete: athlete} do
+      {:ok, gear_live, disconnected_html} = live(conn, "/")
 
-    assert disconnected_html =~ "Hello"
-    assert render(gear_live) =~ "Hello"
+      connected_html = render(gear_live)
+
+      assert disconnected_html =~ "Welcome, #{athlete.first_name}"
+      assert disconnected_html =~ "We are gathering your gear from Strava."
+
+      assert connected_html =~ "Welcome, #{athlete.first_name}"
+      assert connected_html =~ "We are gathering your gear from Strava."
+    end
+
+    test "renders athlete gear", %{conn: conn, athlete: athlete} do
+      gear = insert_list(3, :gear, athlete: athlete)
+
+      {:ok, gear_live, disconnected_html} = live(conn, "/")
+
+      connected_html = render(gear_live)
+
+      assert disconnected_html =~ "Welcome, #{athlete.first_name}"
+
+      assert connected_html =~ "Welcome, #{athlete.first_name}"
+
+      Enum.each(gear, fn g ->
+        assert disconnected_html =~ g.name
+        assert connected_html =~ g.name
+      end)
+    end
   end
 end
