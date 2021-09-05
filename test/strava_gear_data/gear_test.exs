b5d27a000ab8e6d808264e565d74ed51216759bf
SamuelWillis <samuel.w.h.willis@gmail.com>
2021-09-05 15:16:35 -0600

    Add Gear queries
    Add Gear queries that will allow querying against DB for gear.

--- test/strava_gear_data/gear_test.exs
+++ test/strava_gear_data/gear_test.exs
@@ -3,6 +3,36 @@ defmodule StravaGearData.GearTest do
 
   alias StravaGearData.Gear
 
+  describe "get_for!/1" do
+    test "returns single gear" do
+      athlete = build(:athlete) |> with_gear() |> insert()
+
+      assert [gear] = Gear.get_for!(athlete)
+
+      assert is_struct(gear, Gear.Gear)
+      assert gear.athlete_id == athlete.id
+    end
+
+    test "returns many gear" do
+      athlete = build(:athlete) |> with_gear(5) |> insert()
+
+      athlete_gear = Gear.get_for!(athlete)
+
+      assert Enum.count(athlete_gear) == 5
+
+      Enum.each(athlete_gear, fn gear ->
+        assert is_struct(gear, Gear.Gear)
+        assert gear.athlete_id == athlete.id
+      end)
+    end
+
+    test "returns no gear" do
+      athlete = build(:athlete) |> insert()
+
+      assert [] == Gear.get_for!(athlete)
+    end
+  end
+
   describe "insert_all/1" do
     test "inserts new gear" do
       athlete = insert(:athlete)
