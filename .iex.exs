2d7f8f5d52ce0db839db7d8c39fba45002346ab8
SamuelWillis <samuel.w.h.willis@gmail.com>
2021-11-20 17:36:21 -0700

    Update aliased modules in iex sessions

--- .iex.exs
+++ .iex.exs
@@ -1,7 +1,9 @@
 import_if_available(Ecto.Query)
 
+alias StravaGearData.Activities
 alias StravaGearData.Api
 alias StravaGearData.Athletes
 alias StravaGearData.Authorization
 alias StravaGearData.DataCollection
+alias StravaGearData.Gear
 alias StravaGearData.Repo
