aacc43c1a556f9b6886d7591a7218bc4b22f47c6
SamuelWillis <samuel.w.h.willis@gmail.com>
2021-12-27 21:42:41 -0700

    Refactor GearLive into GearLive.Index
    Refactor `GearLive` into `GearLive.Index` so that `GearLive` can house
    shared display concerns for `Gear` views.

---
+++ lib/strava_gear_data_web/live/gear_live/index.ex
@@ -0,0 +1,55 @@
+defmodule StravaGearDataWeb.GearLive.Index do
+  @moduledoc false
+  use StravaGearDataWeb, :live_view
+
+  alias StravaGearData.Athletes
+  alias StravaGearData.DataCollection
+  alias StravaGearData.Gear
+
+  @impl Phoenix.LiveView
+  def mount(_params, _session, socket) do
+    athlete = socket.assigns.athlete
+
+    DataCollection.subscribe(athlete)
+
+    gear = Gear.get_for!(athlete, preload: :activities)
+
+    socket =
+      socket
+      |> assign(:athlete, athlete)
+      |> assign(:gear, gear)
+
+    {:ok, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_params(_params, _uri, socket), do: {:noreply, socket}
+
+  @impl Phoenix.LiveView
+  def handle_event("delete-athlete-data", _params, %{assigns: assigns} = socket) do
+    socket =
+      case Athletes.delete_athlete(assigns.athlete) do
+        {:ok, _deleted_athlete} ->
+          push_redirect(socket, to: Routes.auth_path(socket, :delete))
+
+        {:error, _changeset} ->
+          put_flash(socket, :error, gettext("Error deleting athlete"))
+      end
+
+    {:noreply, socket}
+  end
+
+  def handle_event("gather-athlete-data", _params, %{assigns: assigns} = socket) do
+    DataCollection.gather_athlete_data(assigns.athlete)
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_info({:gather_athlete_data_complete, _athlete}, socket) do
+    athlete = socket.assigns.athlete
+
+    gear = Gear.get_for!(athlete, preload: :activities)
+
+    {:noreply, assign(socket, :gear, gear)}
+  end
+end
